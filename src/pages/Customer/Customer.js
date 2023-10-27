0b82837fb854350362b34c5e1885345a367f5ab8
Fatih Tanrıverdi <117272237+Fatih-Tanriverdi@users.noreply.github.com>
2023-10-27 21:46:33 +0300

    Customer sayfasının UI kısmı güncellendi.

--- src/pages/Customer/Customer.js
+++ src/pages/Customer/Customer.js
@@ -1,6 +1,6 @@
 import "./Customer.css";
 import { Routes, Route, Link } from "react-router-dom";
-import { Row, Col } from 'react-grid-system';
+
 import About from "../../pages/About/About";
 import Planets from '../../pages/Planets/Planets';
 import Iletisim from '../Communication/Iletisim';
@@ -44,49 +44,30 @@ export default function AsideHeader() {
 
     return (
         <container id='customer-panel-body'>
-            <Row className='customer-container-header'>
-                <Col>
-                    <section>
-                        <article className='header-customer'>
-                            <div className='header-position-customer'>
-                                <div className="customer-btn">
-                                    <PageButton id="customer-btn-position" text="Bilet Al" to="ticket" className={`customer-btn-hover ${activeButton === "Bilet Al" ? 'active' : ''}`} onClick={() => handleButtonClick("Bilet Al")} />
-                                    <PageButton id="customer-btn-position" text="Gezegenler" to="planets" className={`customer-btn-hover ${activeButton === "Gezegenler" ? 'active' : ''}`} onClick={() => handleButtonClick("Gezegenler")} />
-                                    <PageButton id="customer-btn-position" text="Hakkımızda" to="about" className={`customer-btn-hover ${activeButton === "Hakkımızda" ? 'active' : ''}`} onClick={() => handleButtonClick("Hakkımızda")} />
-                                    <PageButton id="customer-btn-position" text="İletişim" to="iletisim" className={`customer-btn-hover ${activeButton === "İletişim" ? 'active' : ''}`} onClick={() => handleButtonClick("İletişim")} />
-                                </div>
-                                <div className="customer-exit">
-                                    <div className='customer-info'>
-                                        <h1>{userInfo.name} {userInfo.surname}</h1>
-                                    </div>
-                                    <div>
-                                        <img alt='admin' className='customer-img' src='/images/CustomerPP.png' />
-                                    </div>
-                                    <Link to="/" className="customer-ext-btn">
-                                        <IoExitOutline onClick={handleLogout} alt="Çıkış" />
-                                    </Link>
-                                </div>
-                            </div>
-                        </article>
-                        <article>
-                            <div>
-                                <container className='customer-page-body' >
-                                    <div className='customer-page-container'>
-                                        <Routes>
-                                            <Route path="/*" element={<Ticket />} />
-                                            <Route path="about" element={<About />} />
-                                            <Route path="ticket" element={<Ticket />} />
-                                            <Route path="iletisim" element={<Iletisim />} />
-                                            <Route path="planets" element={<Planets />} />
-                                            <Route path="/planet/:id" element={<PlanetDetails />} />
-                                        </Routes>
-                                    </div>
-                                </container>
-                            </div>
-                        </article>
-                    </section>
-                </Col>
-            </Row>
+            <article className='headerCustomer'>
+                <div className='headerCustomerInfo'>
+                    <h1 className="headerTitleStyle">Arcanis</h1>
+                    <div className="headerButtonContainer">
+                        <Link className="headerButtonStyle">Üye Girişi</Link>
+                        <Link className="headerButtonStyle borderRL">Hakkımızda</Link>
+                        <Link className="headerButtonStyle">İletişim</Link>
+                    </div>
+                </div>
+            </article>
+            <article>
+                <container className='customer-page-body' >
+                    <div className='customer-page-container'>
+                        <Routes>
+                            <Route path="/*" element={<Ticket />} />
+                            <Route path="about" element={<About />} />
+                            <Route path="ticket" element={<Ticket />} />
+                            <Route path="iletisim" element={<Iletisim />} />
+                            <Route path="planets" element={<Planets />} />
+                            <Route path="/planet/:id" element={<PlanetDetails />} />
+                        </Routes>
+                    </div>
+                </container>
+            </article>
         </container>
     )
 }
