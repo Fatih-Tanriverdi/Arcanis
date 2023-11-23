e420ad224047cf8b3a5a023c1e5a92624d6d54c5
Fatih-Tanriverdi <fmehmettanriverdi@gmail.com>
2023-11-24 00:51:38 +0300

    Müşteri sefer listeleme işlemlerinde düzenlemeler

--- src/helpers/utils.jsx
+++ src/helpers/utils.jsx
@@ -1,8 +1,30 @@
 export const displayMoney = (n) => {
-    const format = new Intl.NumberFormat('tr-TR', {
-      style: 'currency',
-      currency: 'try'
-    });
-  
-    return format.format(n);
+  const format = new Intl.NumberFormat("tr-TR", {
+    style: "currency",
+    currency: "try",
+  });
+
+  return format.format(n);
+};
+
+export const displayFullDate = (dateString) => {
+  var dateObject = new Date(dateString);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
   };
+  return Intl.DateTimeFormat("tr-TR", options).format(dateObject);
+};
+
+export const displayDate = (dateString) => {
+  var dateObject = new Date(dateString);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return Intl.DateTimeFormat("tr-TR", options).format(dateObject);
+};
--- src/pages/BuyTicket/BuyTicket.js
+++ src/pages/BuyTicket/BuyTicket.js
@@ -1,113 +1,132 @@
-import React, { useEffect, useState } from 'react';
-import './BuyTicket.css';
-import { Button, Table } from 'antd';
-import { fetchRocketsGet } from '../../services/RocketService';
-import Config from "../../config-file.json"
-import BuyTicketModal from '../../components/buy-ticket-modal/buy-ticket-modal';
+import React, { useEffect, useState } from "react";
+import "./BuyTicket.css";
+import { Button, Table } from "antd";
+import { fetchRocketsGet } from "../../services/RocketService";
+import Config from "../../config-file.json";
+import BuyTicketModal from "../../components/buy-ticket-modal/buy-ticket-modal";
+import { displayFullDate } from "../../helpers/utils";
 
-export default function BuyTicket({ ticketSalesFilter, totalPageCount, pageOdata, pageSizeOdata, setPageOdata, setPageSizeOdata }) {
+export default function BuyTicket({
+  ticketSalesFilter,
+  totalPageCount,
+  pageOdata,
+  pageSizeOdata,
+  setPageOdata,
+  setPageSizeOdata,
+}) {
+  const [spaceVehicleData, setSpaceVehicleData] = useState([]);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [ticketId, setTicketId] = useState();
 
-    const [spaceVehicleData, setSpaceVehicleData] = useState([]);
-    const [isModalVisible, setModalVisible] = useState(false);
-    const [ticketId, setTicketId] = useState();
+  const columns = [
+    {
+      title: "Sefer Adı",
+      dataIndex: "Name",
+      key: "Name",
+    },
+    {
+      title: "Kalkış Tarihi",
+      dataIndex: "ExpeditionDate",
+      key: "ExpeditionDate",
+      render: (expeditionDate, record) => displayFullDate(expeditionDate),
+    },
+    {
+      title: "Varış Tarihi",
+      dataIndex: "ArrivalDate",
+      key: "ArrivalDate",
+      render: (arrivalDate, record) => displayFullDate(arrivalDate),
+    },
+    {
+      title: "Bilet Fiyatı",
+      dataIndex: "TicketPrice",
+      key: "TicketPrice",
+      render: (_, record) => {
+        const roundedTicketPrice = record.TicketPrice.toFixed(2);
+        const formattedTicketPrice = new Intl.NumberFormat("tr-TR", {
+          style: "currency",
+          currency: "TRY",
+        }).format(roundedTicketPrice);
+        return formattedTicketPrice;
+      },
+    },
+    {
+      title: "Uzay Aracı Adı",
+      dataIndex: "SpaceVehicleId",
+      key: "SpaceVehicleId",
+      render: (spaceVehicleData) => mapSpaceVehicleIdToName(spaceVehicleData),
+    },
+    {
+      title: "Satın Al",
+      dataIndex: "",
+      key: "",
+      render: (_, record) => (
+        <Button
+          className="buyButton"
+          type="primary"
+          onClick={() => handleBuyClick(record)}
+        >
+          Satın Al
+        </Button>
+      ),
+    },
+  ];
 
-    const columns = [
-        {
-            title: 'Sefer Adı',
-            dataIndex: 'Name',
-            key: 'Name',
-        },
-        {
-            title: 'Varış Tarihi',
-            dataIndex: 'ArrivalDate',
-            key: 'ArrivalDate',
-            render: (arrivalDate, record) => formatDate(arrivalDate)
-        },
-        {
-            title: 'Bilet Fiyatı',
-            dataIndex: 'TicketPrice',
-            key: 'TicketPrice',
-            render: (_, record) => {
-                const roundedTicketPrice = record.TicketPrice.toFixed(2);
-                const formattedTicketPrice = new Intl.NumberFormat('tr-TR', { style: 'currency', currency: 'TRY' }).format(roundedTicketPrice);
-                return formattedTicketPrice;
-            },
-        },
-        {
-            title: 'Uzay Aracı Adı',
-            dataIndex: 'SpaceVehicleId',
-            key: 'SpaceVehicleId',
-            render: (spaceVehicleData) => mapSpaceVehicleIdToName(spaceVehicleData)
-        },
-        {
-            title: 'Satın Al',
-            dataIndex: '',
-            key: '',
-            render: (_, record) => (
-                <Button className='buyButton' type="primary" onClick={() => handleBuyClick(record)}>
-                    Satın Al
-                </Button>
-            ),
-        },
-    ];
+  /* Sunucudan gelen veriler düzeltilerek kullanıcıya gösteriliyor*/
+  const mapSpaceVehicleIdToName = (spaceVehicleId) => {
+    const spaceVehicle = spaceVehicleData.find(
+      (vehicle) => vehicle.id === spaceVehicleId
+    );
+    return spaceVehicle ? spaceVehicle.displayName : "";
+  };
 
-    /* Sunucudan gelen veriler düzeltilerek kullanıcıya gösteriliyor*/
-    const mapSpaceVehicleIdToName = (spaceVehicleId) => {
-        const spaceVehicle = spaceVehicleData.find(vehicle => vehicle.id === spaceVehicleId);
-        return spaceVehicle ? spaceVehicle.displayName : '';
-    };
+  /* Sunucudan gelen veriler düzeltilerek kullanıcıya gösteriliyor*/
 
-    function formatDate(dateString) {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return new Date(dateString).toLocaleDateString(undefined, options);
+  async function fetchRocketData() {
+    try {
+      const url = `${Config.SERVICE_URL}/lookups/space-vehicles`;
+      const data = await fetchRocketsGet(url);
+      setSpaceVehicleData(data);
+    } catch (error) {
+      console.error("API talebi başarısız oldu: ", error);
     }
-    /* Sunucudan gelen veriler düzeltilerek kullanıcıya gösteriliyor*/
+  }
 
-    /* LOOKUPS */
-    useEffect(() => {
-        async function fetchRocketData() {
-            try {
-                const url = `${Config.SERVICE_URL}/lookups/space-vehicles`;
-                const data = await fetchRocketsGet(url);
-                setSpaceVehicleData(data);
-            } catch (error) {
-                console.error('API talebi başarısız oldu: ', error);
-            }
-        }
-        fetchRocketData();
-    }, []);
-    /* LOOKUPS */
+  /* LOOKUPS */
+  useEffect(() => {
+    fetchRocketData();
+  }, []);
+  /* LOOKUPS */
 
-    const handleBuyClick = (record) => {
-        setModalVisible(true);
-        setTicketId(record.Id);
-    };
+  const handleBuyClick = (record) => {
+    setModalVisible(true);
+    setTicketId(record.Id);
+  };
 
-    return (
-        <div className='buyTicketContainer'>
-            <div className='tableWrapper'>
-                <Table
-                    className='buyTicketTable'
-                    columns={columns}
-                    dataSource={ticketSalesFilter}
-                    rowClassName="tableRow"
-                    pagination={{
-                        total: totalPageCount,
-                        current: pageOdata,
-                        pageSize: pageSizeOdata,
-                        showSizeChanger: true,
-                        onChange: (page, pageSize) => {
-                            setPageOdata(page);
-                            setPageSizeOdata(pageSize);
-                        }
-                    }}
-                />
-            </div>
-            <BuyTicketModal
-                visible={isModalVisible}
-                onClose={() => setModalVisible(false)}
-                ticketId={ticketId}
-            />
-        </div>
-    )
+  return (
+    <div className="buyTicketContainer">
+      <div className="tableWrapper">
+        <Table
+          className="buyTicketTable"
+          columns={columns}
+          dataSource={ticketSalesFilter}
+          rowClassName="tableRow"
+          pagination={{
+            total: totalPageCount,
+            current: pageOdata,
+            pageSize: pageSizeOdata,
+            showSizeChanger: true,
+            onChange: (page, pageSize) => {
+              setPageOdata(page);
+              setPageSizeOdata(pageSize);
+            },
+          }}
+        />
+      </div>
+      <BuyTicketModal
+        visible={isModalVisible}
+        onClose={() => setModalVisible(false)}
+        ticketId={ticketId}
+      />
+    </div>
+  );
 }
